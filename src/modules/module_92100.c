485a9b1b0772a9093d6c3c7ab31cd80d4ed78b46
Reece Dunham <me@rdil.rocks>
2022-10-17 22:59:10 -0400

    fixed some but not all errors

--- src/modules/module_92100.c
+++ src/modules/module_92100.c
@@ -111,6 +111,8 @@ void module_init (module_ctx_t *module_ctx)
   module_ctx->module_interface_version        = MODULE_INTERFACE_VERSION_CURRENT;
 
   module_ctx->module_attack_exec              = module_attack_exec;
+  module_ctx->module_hash_decode_postprocess  = MODULE_DEFAULT;
+  module_ctx->module_benchmark_charset        = MODULE_DEFAULT;
   module_ctx->module_benchmark_esalt          = MODULE_DEFAULT;
   module_ctx->module_benchmark_hook_salt      = MODULE_DEFAULT;
   module_ctx->module_benchmark_mask           = MODULE_DEFAULT;
